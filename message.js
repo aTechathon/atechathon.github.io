2c56e4ddcac168d2aed585bf464e7000118c0821
Theo Armour <t.armour@gmail.com>
2014-05-07 23:48:44 -0700

    typos

--- message.js
+++ message.js
@@ -2,8 +2,8 @@
 		'<hr>' +
 
 		'<h4>Update 2014-05-07</h4>' +
-		'<p>Project launch: <a href="https://www.hackerleague.org/hackathons/aec-technology-hackathon-2014/hacks/atechathon" target="_blank">aTechathon on Hacker League</a></p>' +
-		'<p>dding in read me and menu files.</p>'
+		'<p>Project launch:<br><a href="https://www.hackerleague.org/hackathons/aec-technology-hackathon-2014/hacks/atechathon" target="_blank">aTechathon on Hacker League</a></p>' +
+		'<p>Adding in read me and menu files.</p>' +
 		'<h4>Update 2014-05-05</h4>' +
 		'<p>First Commit</p>' +
 
